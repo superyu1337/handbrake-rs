33e4c429c65ae79f5423a41eb8e788a372926b6a
Reliq Dev <dev@reliq.uk>
2025-07-18 10:40:04 +0100

    Make event_stream a fully working example

--- examples/event_stream.rs
+++ examples/event_stream.rs
@@ -1,12 +1,23 @@
 use std::time::{Duration, Instant};
 
+use clap::Parser;
 use futures::StreamExt;
 use handbrake::{HandBrake, InputSource, JobEvent, OutputDestination};
 use tracing::{error, info};
 
+/// A simple example of using the event stream.
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// The path to the input file.
+    #[arg(short, long)]
+    input: std::path::PathBuf,
+}
+
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
+    let cli = Cli::parse();
 
     // Attempt to discover HandBrakeCLI
     let handbrake = match HandBrake::new().await {
@@ -19,14 +30,12 @@ async fn main() {
     };
     info!("Found HandBrakeCLI: {}", handbrake.version());
 
-    info!("Starting job with a non-existent file to demonstrate error events.");
-    let input = InputSource::from(std::path::PathBuf::from(
-        "completely-random-file-name.mkv",
-    ));
+    info!("Starting job with input file: {:?}", cli.input);
+    let input = InputSource::from(cli.input);
 
     let mut job_handle = match handbrake
         .job(input, OutputDestination::Stdout)
-        .video_codec("qwerty")
+        .preset("Very Fast 1080p30")
         .format("mkv")
         .start()
     {
@@ -49,7 +58,7 @@ async fn main() {
             JobEvent::Done(result) => {
                 info!(?result, "Job finished");
                 break;
-            },
+            }
             JobEvent::Fragment(fragment) => {
                 if then.elapsed() >= Duration::from_secs(10) {
                     info!("Fragment received: {} bytes", fragment.len());
